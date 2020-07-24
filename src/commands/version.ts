d6a51b25f05aa57122ebb2b0dcd0a508272ffd43
Seven Du <shiweidu@outlook.com>
2020-07-25 01:03:27 +0800

    chore: *

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -2,8 +2,6 @@ import { Command } from "../command";
 import { Application } from "..";
 
 export class VersionCommand extends Command {
-    name: string = "version";
-
     async handle(app: Application) {
         return {
             name: app.name,
